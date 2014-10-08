27855957dbe04bd4721fecc1bfb2b511db10c575
Andrew Frauen <frauen.a@gmail.com>
2014-10-08 16:53:34 -0700

    a little bit of clean-up to the script template

--- ruby/template/script.rb
+++ ruby/template/script.rb
@@ -1,6 +1,6 @@
 require 'rubygems'
-require 'bundler'
-Bundler.setup(:default)
+require 'bundler/setup'
+Bundler.require(:default)
 
 require 'yaml'
 
@@ -16,8 +16,22 @@ require 'yaml'
 BASE_PATH = Pathname.new(File.expand_path('../', __FILE__))
 AUTH      = YAML.load_file(BASE_PATH.join('credentials.yml'))
 
-# The 'desk' method returns the client that we will use to issue requests to the
-# Desk API
+# The 'desk' method below returns the client that we will use to issue requests
+# to the Desk API
 def desk
   @desk ||= DeskApi::Client.new(DESK_API)
+end
+
+
+def method_to_be_called_by_task
+  puts "Normally this would be a custom script, but hey, this is just a template"
+end
+
+class Tasks < Thor
+  namespace :script
+
+  desc 'some_task', 'do something'
+  def some_task
+    method_to_be_called_by_task
+  end
 end
